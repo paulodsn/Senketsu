558f1f977965a879562cb4909eea0446e37c66a9
Paulo <pauloricardodsn@gmail.com>
2019-08-24 20:38:30 -0300

    feat: add rollup commonjs in build

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
 import vue from 'rollup-plugin-vue';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default [
     {
-        input: 'src/index.js',
+        input: 'src/App.vue',
         output: {
             format: 'esm',
             file: 'dist/library.esm.js'
         },
         plugins: [
             vue(),
+            commonjs(),
         ]
     },
 ]
